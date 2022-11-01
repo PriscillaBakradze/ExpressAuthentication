45f4b5a70c3cd43b5b8147f0785546b63218f8c9
Priscilla Bakradze <priscillabakradze@hotmail.com>
2022-10-31 21:16:23 -0400

    initial commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/user_collection"
+    //"URI": "mongodb://localhost/user_collection"
+    "URI": "mongodb+srv://Priscilla:oNQemfY4y5OC9YgX@cluster0.tqo1gib.mongodb.net/user_collection?retryWrites=true&w=majority"
 }
